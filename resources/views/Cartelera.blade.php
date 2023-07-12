d0180a09151693c92abc9621d9de75f8243b510c
nediakX <vicentebarraza17@outlook.com>
2023-07-12 11:47:38 -0400

    Update Cartelera.blade.php

--- resources/views/Cartelera.blade.php
+++ resources/views/Cartelera.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Cine MidDA</title>
+    <title>Cine MidDA Cartelera</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
