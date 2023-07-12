6b870b93abf21bb25949cca5af67a5ecbe1bd7fa
nediakX <vicentebarraza17@outlook.com>
2023-07-11 20:21:16 -0400

    npm install

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('Cine Midda', 'Cine Midda') }}</title>
+        <title>{{ config('Cine MidDA', 'Cine MidDA') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
