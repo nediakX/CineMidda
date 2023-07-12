aa134973b87a13ecb9b49344ef74644a512dbced
nediakX <vicentebarraza17@outlook.com>
2023-07-12 05:40:23 -0400

    api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,38 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', [FuncionController::class, 'showWelcome'])->name('welcome');
+
+Route::get('/Funciones/create', [FuncionController::class, 'create'])
+    ->name('Funciones.create');
+
+Route::get('/Funciones/{id}', [FuncionController::class, 'show'])->name('Funciones.show');
+
+Route::get('/Contacto', function () {
+    return view('contacto');
+})->name('contacto');
+
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
+    Route::resource('/Funciones', FuncionController::class);
+
+    Route::delete('/Funciones/{funcion}', 'FuncionController@destroy')->name('Funciones.destroy');
+
+    Route::get('/dashboard', [FuncionController::class, 'validarReservas'])->name('dashboard');
+
+    Route::get('/administracion-user', [AdministracionUserController::class, 'index'])->name('administracion-user.index');
+    Route::get('/administracion-user/{id}/edit', [AdministracionUserController::class, 'edit'])->name('administracion-user.edit');
+    Route::put('/administracion-user/{id}', [AdministracionUserController::class, 'update'])->name('administracion-user.update');
+    Route::delete('/administracion-user/{id}', [AdministracionUserController::class, 'destroy'])->name('administracion-user.destroy');
+
+
+    Route::delete('/Funciones/destroyReserva/{id}', [FuncionController::class, 'destroyReserva'])->name('Funciones.destroyReserva');
+});
+
+Route::get('/Funciones/{id}/reservar', [FuncionController::class, 'reservarAsientos'])->name('Funciones.reservar');
+
+Route::post('/Funciones/{id}/reservar', [FuncionController::class, 'guardarReserva'])->name('funciones.guardarReserva');
+
+Route::post('/Funciones/{id}/ingresardatos', [FuncionController::class, 'ingresardatos'])->name('Funciones.ingresardatos');
+
+Route::get('/Cartelera', [FuncionController::class, 'cartelera'])->name('cartelera');
