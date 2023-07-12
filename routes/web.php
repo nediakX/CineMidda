30758f855c2d58bc605c19ba0cf0e28ac81f471d
nediakX <vicentebarraza17@outlook.com>
2023-07-12 11:44:25 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,8 @@ Route::get('/funciones/create', [FuncionController::class, 'create'])
 
 Route::get('/funciones/{id}', [FuncionController::class, 'show'])->name('funciones.show');
 
-Route::get('/contacto', function () {
-    return view('contacto');
-})->name('contacto');
+Route::get('/contacto', [FuncionController::class, 'contacto'])->name('contacto');
+
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
     Route::resource('/funciones', FuncionController::class);
@@ -48,4 +47,4 @@ Route::post('/funciones/{id}/reservar', [FuncionController::class, 'guardarReser
 
 Route::post('/funciones/{id}/ingresardatos', [FuncionController::class, 'ingresardatos'])->name('funciones.ingresardatos');
 
-Route::get('/Cartelera', [FuncionController::class, 'cartelera'])->name('cartelera');
+Route::get('/cartelera', [FuncionController::class, 'cartelera'])->name('cartelera');
