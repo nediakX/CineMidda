3f1a616af066f0610dfc3ce4b1bead862b91317a
nediakX <vicentebarraza17@outlook.com>
2023-07-16 16:14:49 -0400

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,6 +13,8 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script src="https://cdn.tailwindcss.com"></script>
+
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
