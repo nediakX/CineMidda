524b4d30b0ca06e0375b12553738dd249175ea4e
nediakX <vicentebarraza17@outlook.com>
2023-07-16 18:33:25 -0400

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,17 +4,18 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Iniciar Sesion</title>
-    <link rel="icon" type="image/x-icon" href="/favicon.ico">
-    <link rel="icon" type="image/x-icon" href="/icons/favicon/favicon.ico">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Iniciar Sesión</title>
+    <link rel="icon" type="image/x-icon" href="{{ secure_asset('favicon.ico') }}">
+    <link rel="icon" type="image/x-icon" href="{{ secure_asset('icons/favicon/favicon.ico') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
+        rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
+        crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&family=Jura:wght@500&display=swap"
         rel="stylesheet">
 
-    <link rel="stylesheet" href="/css/styleAdmin.css">
+    <link rel="stylesheet" href="{{ secure_asset('css/styleAdmin.css') }}">
 </head>
 
 <body class="bg-animate">
@@ -25,7 +26,7 @@
                 @csrf
                 <div class="row">
                     <div class="col">
-                        <img src="{{ secure_asset('/images/cine en el midda admin.png') }}" alt="">
+                        <img src="{{ secure_asset('images/cine en el midda admin.png') }}" alt="">
                     </div>
                 </div>
                 <br>
@@ -36,8 +37,8 @@
                 </div>
                 <div class="row">
                     <div class="col">
-                        <input type="email" name="email" id="email" required placeholder="Ingrese su correo electrónico"
-                            autofocus>
+                        <input type="email" name="email" id="email" required
+                            placeholder="Ingrese su correo electrónico" autofocus>
                     </div>
                 </div>
                 <p></p>
@@ -56,9 +57,9 @@
                 <div class="row">
                     <div>
                         <button type="submit" name="enviar" style="color: white;background: #0C6D7E;
-                    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-                    border-radius: 16px; width: 264px;
-                    height: 63px;">Iniciar Sesión</button>
+                            box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+                            border-radius: 16px; width: 264px;
+                            height: 63px;">Iniciar Sesión</button>
                     </div>
             </form>
             <p></p>
@@ -66,7 +67,7 @@
                 ¿Ha olvidado su contraseña? <a href="{{ secure_url('password.request') }}">¡Haga clic aquí!</a>
             </div>
             <div class="register">
-                <a href="{{ secure_url('register') }}">Regístrate</a> | <a href="/">Volver</a>
+                <a href="{{ secure_url('register') }}">Regístrate</a> | <a href="{{ secure_url('/') }}">Volver</a>
             </div>
             <x-validation-errors class="mt-5" />
         </label>
