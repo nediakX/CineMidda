e68c4d1f23cd2ed5eeb0ccbb5e7eac28308b6701
nediakX <vicentebarraza17@outlook.com>
2023-07-16 15:48:49 -0400

    postcss

--- postcss.config.js
+++ postcss.config.js
@@ -4,3 +4,12 @@ export default {
         autoprefixer: {},
     },
 };
+/** @type {import('postcss-load-config').Config} */
+const config = {
+    plugins: [
+      require('autoprefixer'),
+      require('postcss-nested')
+    ]
+  }
+
+  module.exports = config
