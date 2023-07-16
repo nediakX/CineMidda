1ecd6e883ba8cfdc740351cc92ff38d2df8924f8
nediakX <vicentebarraza17@outlook.com>
2023-07-16 16:23:41 -0400

    https://cinemidda-app-n5m8w.ondigitalocean.app/

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://cinemidda-app-n5m8w.ondigitalocean.app/'),
 
     'asset_url' => env('ASSET_URL'),
 
