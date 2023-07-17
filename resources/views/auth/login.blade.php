66ebd4922ea4234c14958073c17abb772316a5d5
nediakX <vicentebarraza17@outlook.com>
2023-07-16 20:31:53 -0400

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,32 +4,17 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Iniciar Sesión</title>
-    <link rel="icon" type="image/x-icon" href="{{ secure_asset('icons/favicon/favicon.ico') }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
-        rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
-        crossorigin="anonymous">
+    <title>Iniciar Sesion</title>
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
+    <link rel="icon" type="image/x-icon" href="/icons/favicon/favicon.ico">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&family=Jura:wght@500&display=swap"
         rel="stylesheet">
 
-    <link rel="stylesheet" href="{{ secure_asset('css/styleAdmin.css') }}">
-    <style>
-        @media (max-width: 768px) {
-            #formulario {
-                width: 90%;
-            }
-
-            #formulario img {
-                width: 60%;
-            }
-
-            #formulario button {
-                width: 100%;
-            }
-        }
-    </style>
+    <link rel="stylesheet" href="/css/styleAdmin.css">
 </head>
 
 <body class="bg-animate">
