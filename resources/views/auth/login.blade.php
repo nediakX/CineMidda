4eee8a41af9dc2a066d095ca32f03e0d92d420c9
nediakX <vicentebarraza17@outlook.com>
2023-07-16 20:24:19 -0400

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Iniciar Sesión</title>
-    <link rel="icon" type="image/x-icon" href="{{ secure_asset('favicon.ico') }}">
     <link rel="icon" type="image/x-icon" href="{{ secure_asset('icons/favicon/favicon.ico') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
         rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
@@ -16,20 +15,32 @@
         rel="stylesheet">
 
     <link rel="stylesheet" href="{{ secure_asset('css/styleAdmin.css') }}">
+    <style>
+        @media (max-width: 768px) {
+            #formulario {
+                width: 90%;
+            }
+
+            #formulario img {
+                width: 60%;
+            }
+
+            #formulario button {
+                width: 100%;
+            }
+        }
+    </style>
 </head>
 
 <body class="bg-animate">
     <div class="d-flex justify-content-center">
-        <div id="formulario">
+        <div id="formulario" class="p-3">
+            <div class="text-center">
+                <img src="{{ secure_asset('images/cine en el midda admin.png') }}" alt="">
+            </div>
             <br>
             <form method="POST" action="{{ secure_url('login') }}">
                 @csrf
-                <div class="row">
-                    <div class="col">
-                        <img src="{{ secure_asset('images/cine en el midda admin.png') }}" alt="">
-                    </div>
-                </div>
-                <br>
                 <div class="row">
                     <div class="col">
                         <p>Correo</p>
@@ -55,12 +66,13 @@
                 </div>
                 <br>
                 <div class="row">
-                    <div>
+                    <div class="col">
                         <button type="submit" name="enviar" style="color: white;background: #0C6D7E;
                             box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-                            border-radius: 16px; width: 264px;
+                            border-radius: 16px; width: 100%;
                             height: 63px;">Iniciar Sesión</button>
                     </div>
+                </div>
             </form>
             <p></p>
             <div class="forget">
@@ -70,7 +82,7 @@
                 <a href="{{ secure_url('register') }}">Regístrate</a> | <a href="{{ secure_url('/') }}">Volver</a>
             </div>
             <x-validation-errors class="mt-5" />
-        </label>
+        </div>
     </div>
     <div class="bubbles">
         <div class="bubble"></div>
