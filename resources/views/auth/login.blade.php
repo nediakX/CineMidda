caacd63747787eb361ff0eb9458415ac20a84ffc
nediakX <vicentebarraza17@outlook.com>
2023-07-17 15:06:31 -0400

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -37,7 +37,6 @@
             border-radius: 8px;
             box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
             margin: 20px;
-            padding: 30px;
         }
 
         #formulario img {
