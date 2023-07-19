f9cb0f89842f17a9b19d7ef8664b5b6f3aa94241
nediakX <vicentebarraza17@outlook.com>
2023-07-19 00:10:09 -0400

    Update ingresardatos.blade.php

--- resources/views/funciones/ingresardatos.blade.php
+++ resources/views/funciones/ingresardatos.blade.php
@@ -85,7 +85,7 @@
                     <h3 style="text-align: center">Asientos seleccionados:</h3>
                     <div id="asientosSeleccionadosContainer">{{ $asientos }}</div>
                     <div class="botonCancelar">
-                        <h5><a href="{{ route('reservar') }}">Elegir nuevamente</a></h5>
+                        <h5><a href="reservar">Elegir nuevamente</a></h5>
                     </div>
 
                 </div>
